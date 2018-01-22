25397c3637d4bf737ba06ceef5065ec2660f0491
Danton Vu <vudanton@gmail.com>
2018-01-22 10:28:54 -0600

    Restructure code to match requirements of stories up to Story ID: #153805996

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //Read user's name and add to welcome message
+        //Customize welcome message
         System.out.println("\nHello, AttendanceApp!");
         System.out.print("\nEnter your name: ");
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         System.out.println("Welcome, " + name + "!\n");
-
-        //Initialize an ArrayList for a list of random absences
+        
+        //Create and output list of absences
         ArrayList<Integer> absentList = new ArrayList<>();
-
-        //Randomly generate values for absences until absentLength is the size of the user's name
         Random rand = new Random();
         for (int i = 0; i < name.length(); i++) {
-            int absences = rand.nextInt(11);
-            absentList.add(absences);
+            absentList.add(rand.nextInt(11));
         }
-
-        //Print the list of absences
-        System.out.println("The absences are: " + absentList);
+        System.out.println("The elements are: " + absentList);
     }
 }
