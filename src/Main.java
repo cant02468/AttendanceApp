0677f044100ab2f413d5b4e97ac5c9212dd692a5
Danton Vu <vudanton@gmail.com>
2018-01-22 10:18:58 -0600

    Add user's name to welcome message.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\n\nHello, AttendanceApp!\n");
+        //Read user's name and add to welcome message
+        System.out.println("\nHello, AttendanceApp!");
+        System.out.print("\nEnter your name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Welcome, " + name + "!\n");
     }
 }
